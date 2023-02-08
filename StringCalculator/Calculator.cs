82f28224a21e862c2e9ec534147a96f0db7824ae
Manish Makwana <manish.makwana@plexure.com>
2023-02-08 16:50:09 +1300

    Add check for custom separator, and include this in separators to use.

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -8,17 +8,38 @@ namespace StringCalculator
 {
     internal class Calculator
     {
+        char[] separators = new char[] { ',', '\n' };
+        string stringNumbers = "";
+
         internal int Add(string numbers)
         {
             // Validate
             if (numbers.Length == 0) return 0;
 
+            this.stringNumbers = numbers;
+
+            // Check for custom separator
+            if (numbers.Length > 2) CheckForCustomSeparator();
+
             // Calculate sum
-            var stringNumbers = numbers.Split(',', '\n');
-            var intNumbers = stringNumbers.Select(int.Parse).ToArray();
-            var sum = intNumbers.Sum();
+            var stringNumberArray = this.stringNumbers.Split(this.separators);
+            var intNumberArray = stringNumberArray.Select(int.Parse).ToArray();
+            var sum = intNumberArray.Sum();
 
             return sum;
+        }
+
+        private void CheckForCustomSeparator()
+        {
+            if (this.stringNumbers.Substring(0, 2) == "//")
+            {
+                var splitInput = stringNumbers.Split('\n');
+                var newSeparator = splitInput[0].Substring(2);
+
+                this.separators = this.separators.Concat(new char[] { char.Parse(newSeparator) }).ToArray();
+                this.stringNumbers = splitInput[1];
+
+            }
         }
     }
 }
